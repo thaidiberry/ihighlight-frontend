8943f3e65a0f83e7ac086bbe67b6e2e9fe123cf8
thaidiberry <thaidiberry@gmail.com>
2025-06-17 15:45:17 -0700

    changed index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+
 import "./index.css";
 import "./styles/icons/icons.css";
 import "./styles/dark.css";
